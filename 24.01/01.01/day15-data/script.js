df8e4bdb541e623484d761fabf179dd0d272da57
donghoonchoi <pitepan3@naver.com>
2024-01-03 17:57:13 +0900

    24.01.03

--- 24.01/01.01/day15-data/script.js
+++
@@ -1,7 +0,0 @@
-const root = document.querySelector(":root");
-let buttons = document.querySelectorAll("[data-name]")
-buttons.forEach( ( button ) => {
-	button.addEventListener( "click", () => {
-		root.classList.toggle(button.dataset.name)
-	})
-})
